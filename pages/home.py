e221dc20679c581cec61daf18b96158318645121
Michal <Kraninger.M@seznam.cz>
2025-02-26 12:02:57 +0100

    Flashing Buttons #2

--- pages/home.py
+++ pages/home.py
@@ -108,7 +108,14 @@ children = [
             dcc.Interval(
                 id='interval-component',
                 interval=1*1000,  # 1 sekunda v milisekundách
-                n_intervals=0  # Počáteční počet intervalů
+                n_intervals=0,  # Počáteční počet intervalů
+            ),
+
+            dcc.Interval(
+                id='interval-component2',
+                interval=1*750,  # 1 sekunda v milisekundách
+                n_intervals=0,  # Počáteční počet intervalů
+                # disabled=True
             ),
 
         html.Div(
@@ -135,17 +142,19 @@ children = [
                     (
                         children=[
                                     # html.Img(src='assets/nurse.svg', id = 'img1',),
-                                    html.A(html.Img(src='assets/nurse.svg', id = 'img1',), href='/page1', style={'color': 'yellow'})
+                                    # html.A(html.Img(src='assets/nurse.svg', id = 'img1',), href='/page1', style={'color': 'yellow'})
+                                    html.A(html.Img(src='assets/nurse.svg', id = 'img1',))
                                 ],
                       
                         id='c1',
                         className='',
+                        style={'backgroundColor': 'transparent', 'border': 'none'}
                     ),
                     html.Button
                     (
                         children=[
                                     # html.Img(src='assets/caduceus.svg', id = 'img2')
-                                    html.A( html.Img(src='assets/caduceus.svg', id = 'img2'), href='/page2', style={'color': 'yellow'})
+                                    html.A( html.Img(src='assets/caduceus.svg', id = 'img2'), href='/page2')
                                 ],
                         id='c2',
                         className='',
@@ -154,7 +163,7 @@ children = [
                     (
                         children=[
                                     # html.Img(src='assets/nurse2.svg',  id = 'img3'),
-                                    html.A( html.Img(src='assets/nurse2.svg',  id = 'img3'), href='/key', style={'color': 'yellow'})
+                                    html.A( html.Img(src='assets/nurse2.svg',  id = 'img3'), href='/key')
                                 ],
                         id='c3',
                         className='',
@@ -164,7 +173,7 @@ children = [
                         children=[
                                 
                                 # html.Img(src='assets/nurse3.svg',  id = 'img4'),
-                                html.A( html.Img(src='assets/nurse3.svg',  id = 'img4'), href='/db', style={'color': 'yellow'})
+                                html.A( html.Img(src='assets/nurse3.svg',  id = 'img4'), href='/db')
                             ],
                         id='c4',
                         className='',
@@ -236,3 +245,57 @@ def toggle_image_class(n_clicks1, n_clicks2, n_clicks3, n_clicks4):
         classes[3] = 'active-img4'
 
     return classes
+
+
+# ************************ Experiment Blikacka *************************************
+
+@callback(
+    [Output('c1', 'style'),
+     Output('img1', 'style'),
+     Input('interval-component2', 'n_intervals')]
+)
+
+def update_button(n):
+
+    print(f"Interval count: {n}")
+
+    if n == 0:  # Pokud je n 0, znamená to, že interval je zakázán
+        return {'backgroundColor': 'white', 'border': 'none'}, {'filter': ' brightness(0) saturate(100%) invert(27%) sepia(50%) saturate(5281%) hue-rotate(349deg) brightness(88%) contrast(85%)'}
+
+    if n % 2 == 0:  # Změna barvy každé 2 intervaly (tj. každou sekundu)
+        button_style = {'backgroundColor':'red', 'border': 'none'}
+        img_style = {'filter': 'invert(100%)'}
+    else:
+        button_style = {'backgroundColor': 'white', 'border': 'none'}
+        img_style = {'filter': ' brightness(0) saturate(100%) invert(27%) sepia(50%) saturate(5281%) hue-rotate(349deg) brightness(88%) contrast(85%)'}
+
+
+    # print(f"Button style: {button_style}, Image style: {img_style}") 
+
+    return button_style, img_style
+
+
+@callback(
+    Output('interval-component2', 'disabled'),
+    Input('c1', 'n_clicks'),
+    State('interval-component2', 'disabled')
+)
+
+def toggle_interval(n_clicks, disabled):
+    if n_clicks == 0:
+        # Pokud bylo tlačítko nikdy nekliknuto, ponechejte interval zakázaný
+        return True
+    # Při každém kliknutí přepněte stav disabled
+    return not disabled
+
+
+@callback(
+    Output('interval-component2', 'n_intervals'),
+    Input('interval-component', 'disabled'),
+    State('interval-component2', 'n_intervals')
+)
+def reset_interval(disabled, n):
+    if disabled:
+        return 0  # Resetuje n_intervals, pokud je interval zakázaný
+    return n  # Jinak vrátí aktuální n_interval
+ 
